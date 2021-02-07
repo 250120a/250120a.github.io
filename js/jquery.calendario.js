b611bf08cf0d208079a5b765dadd850df6334a95
250120a <73027073+250120a@users.noreply.github.com>
2021-02-08 00:44:35 +0500

    Add files via upload

--- js/jquery.calendario.js
+++ js/jquery.calendario.js
@@ -1,4 +1,13 @@
-
+/**
+ * jquery.calendario.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2012, Codrops
+ * http://www.codrops.com
+ */
 ;( function( $, window, undefined ) {
 	
 	'use strict';
